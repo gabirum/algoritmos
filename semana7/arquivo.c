44da8a8e4ed095f6cdeece9cf60c5d52bc8007cf
gabirum <gabrielmc15@outlook.com>
2021-04-01 23:38:15 -0400

    finalização do arquivo de agenda

--- semana7/arquivo.c
+++ semana7/arquivo.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 #define FILE_PATH "pessoas.txt"
 
@@ -25,7 +26,7 @@ void removePerson(size_t id, Person **people);
 void loadFromFile(Person **people);
 void writeToFile(Person *people);
 
-int isValid(Person *person);
+int isValidPerson(Person *person);
 void copyTo(Person *destination, Person *source);
 
 size_t lastId = 0;
@@ -52,17 +53,25 @@ int main(int argc, char const *argv[])
       memset(&person, 0, sizeof(Person));
 
       printf("Digite o nome:\n");
-      scanf("%s", person.name);
+      scanf("%*c");
+      scanf("%[^\n]", person.name);
       printf("Digite o telefone:\n");
       scanf("%s", person.telephone);
       printf("Digite o email:\n");
       scanf("%s", person.email);
-      printf("Digite a data de nascimento:\n");
+      printf("Digite a data de nascimento (dd/mm/aaaa):\n");
       scanf("%s", person.dateOfBirth);
       printf("Digite o genero:\n");
       scanf("%s", person.gender);
 
-      addPerson(&people, &person);
+      if (isValidPerson(&person))
+      {
+        addPerson(&people, &person);
+      }
+      else
+      {
+        printf("Erro de validação\n");
+      }
       break;
     }
 
@@ -73,18 +82,26 @@ int main(int argc, char const *argv[])
 
       printf("Digite o id:\n");
       scanf("%d", &person.id);
+
+      printf("Preencha o que deseja alterar, se não deixe vazio\n");
       printf("Digite o nome:\n");
-      scanf("%s", person.name);
+      scanf("%*c");
+      scanf("%[^\n]", person.name);
+      scanf("%*c");
       printf("Digite o telefone:\n");
-      scanf("%s", person.telephone);
+      scanf("%[^\n]", person.telephone);
+      scanf("%*c");
       printf("Digite o email:\n");
-      scanf("%s", person.email);
-      printf("Digite a data de nascimento:\n");
-      scanf("%s", person.dateOfBirth);
+      scanf("%[^\n]", person.email);
+      scanf("%*c");
+      printf("Digite a data de nascimento (dd/mm/aaaa):\n");
+      scanf("%[^\n]", person.dateOfBirth);
+      scanf("%*c");
       printf("Digite o genero:\n");
-      scanf("%s", person.gender);
+      scanf("%[^\n]", person.gender);
 
       editPerson(people, &person);
+
       break;
     }
 
@@ -201,7 +218,11 @@ void loadFromFile(Person **people)
 {
   FILE *file = fopen(FILE_PATH, "r");
 
-  if (file != NULL)
+  if (file == NULL)
+  {
+    printf("O arquivo não existe, ele será criado no final da execução do programa\n\n");
+  }
+  else
   {
     Person person;
 
@@ -243,19 +264,83 @@ void writeToFile(Person *people)
 
 void copyTo(Person *destination, Person *source)
 {
-  strcpy(destination->name, source->name);
-  strcpy(destination->email, source->email);
-  strcpy(destination->telephone, source->telephone);
-  strcpy(destination->dateOfBirth, source->dateOfBirth);
-  strcpy(destination->gender, source->gender);
+  if (strlen(source->name) > 0)
+    strcpy(destination->name, source->name);
+
+  if (strlen(source->email) > 0)
+    strcpy(destination->email, source->email);
+
+  if (strlen(source->telephone) > 0)
+    strcpy(destination->telephone, source->telephone);
+
+  if (strlen(source->dateOfBirth) > 0)
+    strcpy(destination->dateOfBirth, source->dateOfBirth);
+
+  if (strlen(source->gender) > 0)
+    strcpy(destination->gender, source->gender);
 }
 
-int isValid(Person *person)
+void sliceStr(char *_buffer, char *_str, size_t _initialPos, size_t _finalPos)
+{
+  int j = 0;
+  for (int i = _initialPos; i < _finalPos; i++)
+  {
+    _buffer[j++] = _str[i];
+  }
+  _buffer[j] = 0;
+}
+
+typedef struct tm Time;
+
+int isValidDate(char _date[11])
+{
+  if (strlen(_date) < 10 || strlen(_date) > 11)
+    return 0;
+
+  char sday[3];
+  char smonth[3];
+  char syear[5];
+  sliceStr(sday, _date, 0, 2);
+  sliceStr(smonth, _date, 3, 5);
+  sliceStr(syear, _date, 6, 10);
+
+  int day = atoi(sday);
+  int month = atoi(smonth);
+  int year = atoi(syear);
+
+  if (day <= 0 || day > 31 || month <= 0 || month > 12)
+    return 0;
+
+  Time date;
+  date.tm_mday = day;
+  date.tm_mon = month - 1;
+  date.tm_year = year - 1900;
+  date.tm_hour = 0;
+  date.tm_min = 0;
+  date.tm_sec = 1;
+  date.tm_isdst = -1;
+
+  time_t timestamp = mktime(&date);
+
+  if (timestamp <= -1)
+    return 0;
+
+  time_t currentTime;
+
+  time(&currentTime);
+
+  if (timestamp < currentTime)
+    return 1;
+
+  return 0;
+}
+
+int isValidPerson(Person *person)
 {
   int nameLen = strlen(person->name);
   int telLen = strlen(person->telephone);
   int mailLen = strlen(person->email);
   int isValidGender = strcmp(person->gender, "masculino") || strcmp(person->gender, "feminino");
 
-  return nameLen > 2 && telLen >= 10 && telLen <= 11 && mailLen > 10 && isValidGender;
+  return nameLen > 2 && telLen >= 10 && telLen <= 11 && mailLen > 10 && isValidGender && isValidDate(person->dateOfBirth);
 }
